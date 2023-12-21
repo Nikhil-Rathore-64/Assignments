996a8eb03cb6fc4c77edce24c8a744ab3e3bfe17
Nikhil-Rathore-64 <rathorenikhil641@gmail.com>
2023-12-21 20:10:35 +0530

    Initial commit

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,13 @@
 */
 
 function findLargestElement(numbers) {
-    
+    let c=numbers[0];
+    for(let i=1;i<numbers.length;i++){
+        if(numbers[i]>c){
+            c=numbers[i];
+        }
+    }
+    return c;
 }
 
 module.exports = findLargestElement;
